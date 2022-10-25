87702aa33b31afc25c05804d77b22ce2ec83ce72
gapatinier <112831239+gapatinier@users.noreply.github.com>
2022-10-25 12:39:15 +0200

    Update model.py

--- model.py
+++ model.py
@@ -4,9 +4,10 @@ class Piece:
         self._color = color
         self._row = i
         self._col = j
-    
+        self._alive = 1
+
     def move(self, board, i, j, game):
-        if [i, j] in defend(self, board):
+        if [i, j] in self.defend(board):
             board.board[self._row][self._col] = Pawn(None, self._row, self._col)
             eaten_piece = board.board[i][j]
             board.board[i][j] = self
@@ -15,13 +16,12 @@ class Piece:
                 self._col = j
                 return True
             else:
-                 board.board[self._row][self._col] = self
-                 board.board[i][j] = board.board[i][j]
-                return False
-     
-    def coords(self):
+                board.board[self._row][self._col] = self
+                board.board[i][j] = eaten_piece
+            return False
+
+    def cords(self):
         return [self._row, self._col]
-            
 
 
 class King(Piece):
@@ -147,7 +147,7 @@ class Knight(Piece):
         defend = []
         for i in krange:
             for j in krange:
-                if (i+j) % 2 == 1:
+                if (i + j) % 2 == 1:
                     row = self._row + i
                     col = self._col + j
                     if -1 < row < 8 and -1 < col < 8:
@@ -184,13 +184,12 @@ class Board:
         color = 1
         self._board[7] = [Rook(color, 7, 0), Rook(color, 7, 7), Knight(color, 7, 1), Knight(color, 7, 6),
                           Bishop(color, 7, 2), Bishop(color, 7, 5), Queen(color, 7, 3), King(color, 7, 4)]
-        self._baord[6] = [Pawn(color, 6, 0), Pawn(color, 6, 1), Pawn(color, 6, 2), Pawn(color, 6, 3),
+        self._board[6] = [Pawn(color, 6, 0), Pawn(color, 6, 1), Pawn(color, 6, 2), Pawn(color, 6, 3),
                           Pawn(color, 6, 4), Pawn(color, 6, 5), Pawn(color, 6, 6), Pawn(color, 6, 7)]
-        
 
     def board(self):
         return self._board
-    
+
     def check_legal_move(self, game):
         if self._turn == 0:
             player1 = game.player1
@@ -198,16 +197,14 @@ class Board:
         else:
             player1 = game.player2
             player2 = game.player1
-        
-        Kingcoords = player1.pieces[7].coords
-        
+
+        king_cords = player1.pieces[7].coords
+
         for piece in player2.pieces:
-            if Kingcoords in piece.defend(self):
+            if king_cords in piece.defend(self):
                 return False
-        
+
         return True
-            
-        
 
 
 class Player:
@@ -224,30 +221,28 @@ class Player:
                             Bishop(color, 7, 2), Bishop(color, 7, 5), Queen(color, 7, 3), King(color, 7, 4),
                             Pawn(color, 6, 0), Pawn(color, 6, 1), Pawn(color, 6, 2), Pawn(color, 6, 3),
                             Pawn(color, 6, 4), Pawn(color, 6, 5), Pawn(color, 6, 6), Pawn(color, 6, 7)]
-    
+
     def pieces(self):
         return self._pieces
 
 
 class HumanPlayer(Player):
-
     pass
 
 
 class ComputerPlayer(Player):
-
     pass
 
 
 class Game:
-    
+
     def __init__(self):
         self._board = Board(0)
         self._player1 = Player
         self._player2 = Player
-    
+
     def player1(self):
         return self._player1
-    
+
     def player2(self):
         return self._player2
