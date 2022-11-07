8756bd0dd796c14f123b4455da51ce8b0f0c5fd5
Ghadi Zaidan <ghadizaidan@gmail.com>
2022-11-07 15:50:38 +0100

    add uml diagram

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import pygame
 class Piece:
     """
     A piece is defined by :
-    - its color (0 for white, 1 for black, None if it belongs to noone or is dead)
+    - its color (0 for white, 1 for black, None if it belongs to no one or is dead)
     - its type (str)
     - its black and white images for display
     - wether or not it has moved for castling rights and for pawns
